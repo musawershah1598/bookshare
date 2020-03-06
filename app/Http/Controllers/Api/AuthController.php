d5f4151f55f2b5948227f96e3fda57ba0d6890b0
Musawer Shah <musawershah1598@gmail.com>
2020-03-06 14:22:13 +0500

    changes to auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,27 +16,35 @@ class AuthController extends Controller
     {
         $validators = Validator::make($request->all(), [
             'name' => "required",
-            'email' => "required|email",
+            'email' => "required|email|unique:users,email",
             'password' => "required|min:3",
             'confirm_password' => 'required|same:password',
         ]);
         if ($validators->fails()) {
-            return response()->json(['error' => $validators->errors()], 401);
+            return response()->json($validators->errors()->all(), 401);
         }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] = $user->createToken("Laravel")->accessToken;
+        $success['message']="Account Created Successfully";
         return response()->json(['success' => $success], $this->successStatus);
     }
 
     public function login(Request $request)
     {
+        $validators = Validator::make($request->all(),[
+            'email'=>"required|email",
+            "password"=>"required"
+        ]);
+        if($validators->fails()){
+            return response()->json($validators->errors()->all(),400);
+        }
         if (Auth::attempt(["email" => $request->get('email'), 'password' => $request->get('password')])) {
             $user = Auth::user();
             $success['token'] = $user->createToken("Laravel")->accessToken;
 			$success['user_id'] = $user->id;
-            return response()->json(['success' => $success], $this->successStatus);
+            return response()->json($success, $this->successStatus);
         } else {
             return response()->json(['error' => "Unauthorized access"], 401);
         }
