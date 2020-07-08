e68a9f35f4da62aab9e3b9dc1fccb7162740eaa2
Musawer Shah <musawershah1598@gmail.com>
2020-07-08 22:22:52 +0500

    remove auth from some routes

--- routes/api.php
+++ routes/api.php
@@ -16,21 +16,30 @@ Route::group(["namespace" => "Api"], function () {
     Route::post("auth/register", "AuthController@register");
     Route::get("auth/email/verify","AuthController@verify")->name("api.email.verify");
     Route::post("auth/email/resend","AuthController@resendEmail");
+
+    // authors
+    Route::get('/authors',"BookController@getAuthors");
+
+    // books route
+    Route::get('/book',"BookController@getbook");
+    Route::get("/books","BookController@getbooks");
+    Route::get("/books/search","BookController@search");
+    Route::get('/books/all',"BookController@allBooks");
+
+    // genres
+    Route::get("/genre","GenreController@index");
+    Route::get("/genre/books","GenreController@books");
+
+
     Route::group(['middleware'=>"auth:api"],function(){
         Route::get('/user', "AuthController@getUser");
         Route::put("/user","AuthController@updateUser");
         Route::post('/user/image/upload',"AuthController@uploadimage");
 
-        // authors
-        Route::get('/authors',"BookController@getAuthors");
 
-        // books route
-        Route::get('/book',"BookController@getbook");
-        Route::get("/books","BookController@getbooks");
-        Route::get("/books/search","BookController@search");
+        
 		Route::get("/books/addview","BookController@addview");
         Route::get("/books/adddownload","BookController@adddownload");
-        Route::get('/books/all',"BookController@allBooks");
 
         // reviews
         Route::get("/review","ReviewController@getReviews");
@@ -41,9 +50,5 @@ Route::group(["namespace" => "Api"], function () {
         Route::get('/bookmark/check',"BookmarkController@checkbookmark");
         Route::post("/bookmark/add","BookmarkController@add");
         Route::post('bookmark/remove',"BookmarkController@removebookmark");
-
-        // genres
-        Route::get("/genre","GenreController@index");
-        Route::get("/genre/books","GenreController@books");
     });
 });
